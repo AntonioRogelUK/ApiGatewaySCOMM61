d97e2ed26babac37c7cab517cf139a392e836791
Juan Martinez <jfmg.64@gmail.com>
2024-06-25 22:48:13 -0700

    Añadido crearVenta en Gateway. Añadido obtenerPorId en Juegos.

---
+++ ApiGateway.Gateway/Controllers/VentasController.cs
@@ -0,0 +1,74 @@
+﻿using ApiGateway.Gateway.Dtos;
+using ApiGateway.Gateway.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Text;
+using System.Text.Json;
+
+namespace ApiGateway.Gateway.Controllers
+{
+    [ApiController]
+    [Route("gateway/[controller]")]
+    public class VentasController : ControllerBase
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public VentasController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<List<VentaDto>>> ObtenerTodos()
+        {
+            var clienteSqlServer = _httpClientFactory.CreateClient(ApiClients.SqlServer.ToString());
+            var response = await clienteSqlServer.GetAsync($"api/Ventas/");
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest();
+            }
+
+            var content = await response.Content.ReadAsStringAsync();
+            var options = new JsonSerializerOptions()
+            {
+                PropertyNameCaseInsensitive = true
+            };
+            var result = JsonSerializer.Deserialize<List<VentaDto>>(content, options);
+
+            return Ok(result);
+        }
+
+
+
+        [HttpPost]
+        public async Task<ActionResult> Crear([FromBody]VentaDto venta)
+        {
+            var clienteSqlServer = _httpClientFactory.CreateClient(ApiClients.SqlServer.ToString());
+            var clienteMondoDB = _httpClientFactory.CreateClient(ApiClients.MongoDB.ToString());
+
+            var juegoResponse = await clienteMondoDB.GetAsync($"api/Juegos/{venta.JuegoId}");
+            var usuarioResponse = await clienteSqlServer.GetAsync($"api/Usuarios/{venta.UsuarioId}");
+
+            if (!juegoResponse.IsSuccessStatusCode)
+            {
+                return NotFound("No se encontró juego");
+            } else if (!usuarioResponse.IsSuccessStatusCode)
+            {
+                return NotFound("No se encontró usuario");
+            }
+
+
+
+            var ventaContent = new StringContent(
+                JsonSerializer.Serialize(venta), Encoding.UTF8, "application/json");
+            var response = await clienteSqlServer.PostAsync("api/Ventas", ventaContent);
+
+            if (!response.IsSuccessStatusCode)
+            {
+                return BadRequest();
+            }
+
+            return Ok();
+        }
+    }
+}
---
+++ ApiGateway.Gateway/Dtos/VentaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiGateway.Gateway.Dtos
+{
+    public class VentaDto
+    {
+        public Guid Id { get; private set; } = Guid.NewGuid();
+        public Guid UsuarioId { get; set; }
+        public string JuegoId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+    }
+}
